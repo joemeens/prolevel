ad999094d97473fd6aab237e1d7270943519551c
joemeens <51346104+joemeens@users.noreply.github.com>
2019-08-06 20:42:01 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,8 +1,8 @@
-n, mass = [int(i) for i in input(" ").split(" ")]
+n, m = [int(i) for i in input(" ").split(" ")]
 u = []
-Lis = [int(i) for i in input().split()]
-for _ in range(mass):
+List1 = [int(i) for i in input().split()]
+for _ in range(m):
     l, k = [int(i) for i in input().split()]
-    u.append(min(Lis[l-1:k]))
+    u.append(min(List1[l-1:k]))
 for i in u:
     print(i)
