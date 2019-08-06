18a92ec186877e5beab3eb706e4dd60d91582c84
joemeens <51346104+joemeens@users.noreply.github.com>
2019-08-06 20:37:55 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,6 +1,6 @@
-nun, mass = [int(i) for i in input(" ").split(" ")]
+n, mass = [int(i) for i in input(" ").split(" ")]
 u = []
-List1 = [int(i) for i in input().split()]
+Lis = [int(i) for i in input().split()]
 for _ in range(mass):
     l, k = [int(i) for i in input().split()]
     u.append(min(Lis[l-1:k]))
